274383f7d0ac47f552aad05b6952bd28a0f1924f
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-11-03 18:07:45 -0400

    more outputs

--- __main__.py
+++ __main__.py
@@ -34,7 +34,48 @@ def get_graph(**options):
     graph.add_chain(
         not_foils,
         not_rares,
-        bonobo.CsvWriter("DeckedBuilder-commons.csv"),
+        bonobo.CsvWriter(path="DeckedBuilder-commons.csv"),
+        _input=split,
+    )
+
+    # MTG Studio
+    graph.add_chain(
+        bonobo.Rename(
+            Name='Card',
+            Edition='Set',
+            Qty='Reg Qty',
+            Foil='Foil Qty',
+        ),
+        bonobo.CsvWriter("MTG-Studio.csv"),
+        _input=split,
+    )
+
+    graph.add_chain(
+        not_foils,
+        not_rares,
+        a_lot,
+        bonobo.OrderFields([
+            'Card',
+            'Set',
+            'Foil Qty',
+            'Reg Qty',
+        ]),
+        bonobo.CsvWriter("bulk.csv"),
+        _input=split,
+    )
+
+    graph.add_chain(
+        tradeable,
+        bonobo.UnpackItems(0),
+        #bonobo.PrettyPrinter(),
+        #bonobo.Limit(3000),
+        bonobo.OrderFields([
+            'Card',
+            'Set',
+            'Foil',
+            'Quantity',
+        ]),
+        bonobo.CsvWriter("CardKingdom-buylist.csv"),
         _input=split,
     )
 
@@ -60,6 +101,69 @@ def get_graph(**options):
 
 from bonobo.config import use, use_context, use_raw_input
 from bonobo.constants import NOT_MODIFIED
+
+
+@use_raw_input
+def a_lot(row):
+    qty = int(row.get('Total Qty'))
+
+    if qty > 16:
+        return NOT_MODIFIED
+
+
+@use_raw_input
+def more_than_set(row):
+
+    qty = int(row.get('Reg Qty'))
+
+    if qty > 8:
+        yield {
+            **row._asdict(),
+            'Reg Qty': qty - 8,
+        }
+
+
+@use_raw_input
+def tradeable(row):
+
+    qty = int(row.get('Reg Qty'))
+    foil_qty = int(row.get('Foil Qty'))
+
+    rarity = row.get('Rarity')
+
+    foil_cutoff = 1
+
+    if rarity == "Rare" or rarity == "Mythic Rare":
+        qty_cutoff = 1
+    else:
+        qty_cutoff = 8
+
+    if qty > qty_cutoff:
+        qty -= qty_cutoff
+    else:
+        qty = 0
+
+    if foil_qty > foil_cutoff:
+        foil_qty -= foil_cutoff
+    else:
+        foil_qty = 0
+
+    price_str = row.get('Single Price') or "0"
+    price = float(price_str)
+
+    if (foil_qty > 0):
+        yield {
+            **row._asdict(),
+            'Quantity': foil_qty,
+            'Foil': 1,
+        }
+
+    if (qty > 0 and price > 0):
+        yield {
+            **row._asdict(),
+            'Quantity': qty,
+            'Foil': 0,
+        }
 
 
 @use_raw_input
